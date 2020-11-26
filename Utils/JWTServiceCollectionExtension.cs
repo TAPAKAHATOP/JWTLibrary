4325259766b3e1c1f09f5da0da046cd0045cc31c
Igrevskiy IO <sirtapakahatop@gmail.com>
2020-11-26 23:31:33 +0300

    Done Server part;

--- Utils/JWTServiceCollectionExtension.cs
+++ Utils/JWTServiceCollectionExtension.cs
@@ -3,10 +3,12 @@ using JWTLibrary.Client;
 using JWTLibrary.Default.Service;
 using JWTLibrary.Default.Service.Client;
 using JWTLibrary.Default.Service.Encrypting;
+using JWTLibrary.Default.Service.Server;
 using JWTLibrary.Default.Service.Signing;
 using JWTLibrary.Interface;
 using JWTLibrary.Interface.Encrypting;
 using JWTLibrary.Interface.Signing;
+using JWTLibrary.Server;
 using JWTLibrary.Utils.Options;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,16 @@ namespace JWTLibrary.Utils
 {
     public static class JWTServiceCollectionExtension
     {
+        public static IServiceCollection AddJWTService(this IServiceCollection services, bool useDefaults = true)
+        {
+            if (useDefaults)
+            {
+                services.AddSingleton<IJWTEncryptingEncodingKeyService, JWTEncryptingEncodingKeyService>();
+                services.AddSingleton<IJWTSigningEncodingKeyService, JWTSigningEncodingKeyService>();
+                services.AddSingleton<IJWTGeneratorService, JWTGeneratorService>();
+            }
+            return services;
+        }
         public static IServiceCollection AddJWTAuthentication(this IServiceCollection services, bool useDefaults = true)
         {
             services.AddSingleton<IJWTUserDataResolver, JWTUserDataResolver>();
